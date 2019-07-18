dc5f05878aee19be1153d3d120dc810ed4e9926a
HieuThao <you@example.com>
2019-07-18 12:39:19 +0700

    bt2

--- src/main/java/com/codegym/controller/CityController.java
+++ src/main/java/com/codegym/controller/CityController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,25 +27,77 @@ public class CityController {
     private CountryService countryService;
 
     @ModelAttribute("countries")
-    public Page<Country> countries(Pageable pageable){
+    public Page<Country> countries(Pageable pageable) {
         return countryService.findAll(pageable);
     }
 
     @GetMapping("/city")
-    public ModelAndView listCity(@PageableDefault(size = 3) Pageable pageable){
+    public ModelAndView listCity(@PageableDefault(size = 3) Pageable pageable) {
         Page<City> cities = cityService.findAll(pageable);
         ModelAndView modelAndView = new ModelAndView("/city/List");
-        modelAndView.addObject("city",cities);
+        modelAndView.addObject("city", cities);
         return modelAndView;
     }
 
     @GetMapping("detail-city/{id}")
-    public ModelAndView detail(@PathVariable Long id , HttpServletRequest httpServletRequest){
+    public ModelAndView detail(@PathVariable Long id, HttpServletRequest httpServletRequest) {
         City city = cityService.findById(id);
         ModelAndView modelAndView = new ModelAndView("/city/Detail");
-        modelAndView.addObject("city",city);
+        modelAndView.addObject("city", city);
         String r = httpServletRequest.getHeader("Referer");
-        modelAndView.addObject("referer",r);
+        modelAndView.addObject("referer", r);
+        return modelAndView;
+    }
+
+    @GetMapping("/create-city")
+    public ModelAndView createCity() {
+        ModelAndView modelAndView = new ModelAndView("/city/Create");
+        modelAndView.addObject("city", new City());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-city")
+    public ModelAndView showCreateCity(@ModelAttribute("city") City city) {
+        cityService.save(city);
+        ModelAndView modelAndView = new ModelAndView("/city/Create");
+        modelAndView.addObject("city", new City());
+        return modelAndView;
+    }
+
+    @GetMapping("/delete-city/{id}")
+    public ModelAndView delete(@PathVariable Long id) {
+        City city = cityService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("/city/Delete");
+        if (city != null) {
+            modelAndView.addObject("city", city);
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/delete-city")
+    public String deleteNote(City city) {
+        cityService.remove(city.getId());
+        return "redirect:/city";
+    }
+
+    @GetMapping("/edit-city/{id}")
+    public ModelAndView edit(@PathVariable Long id) {
+        City city = cityService.findById(id);
+        if (city != null) {
+            ModelAndView modelAndView = new ModelAndView("/city/Edit");
+            modelAndView.addObject("city", city);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-city")
+    public ModelAndView update(@ModelAttribute("city") City city) {
+        cityService.save(city);
+        ModelAndView modelAndView = new ModelAndView("/city/Edit");
+        modelAndView.addObject("city", city);
         return modelAndView;
     }
 
